06785335ab8f71ff6f73b68cdcdfcdc26a4566fd
mayoe <mayoze.jm@googlemail.com>
2020-04-23 14:03:51 +0100

    setup environment

---
+++ index.js
@@ -0,0 +1,10 @@
+import Discord from 'discord.js';
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot loaded succesfully');
+});
+
+// TODO: get token from discord client to add here
+client.login('token');
